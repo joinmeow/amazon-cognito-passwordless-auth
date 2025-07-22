1a39f310067dd8c6fd9636f6cdbcecabb8758df0
Amit <amit@meow.com>
2025-07-22 14:23:42 -0400

    test: verify prettier in pre-commit

---
+++ test-prettier.ts
@@ -0,0 +1,10 @@
+// Test file with bad formatting
+const x = 5;
+const y = 10;
+console.log(x + y);
+function test() {
+  console.log("bad formatting");
+}
+test();
+const obj = { a: 1, b: 2, c: 3 };
+console.log(obj);
